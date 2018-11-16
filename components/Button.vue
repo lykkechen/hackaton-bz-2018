052e7b9eae75f6461f106a420e9c92165af3e125
Samantha Baita <baita.samantha@gmail.com>
2018-11-16 22:16:23 +0100

    :sparkles: Add button component

---
+++ components/Button.vue
@@ -0,0 +1,29 @@
+<template>
+  <nuxt-link
+    @click="onClick"
+    :to="to"
+    class="button">
+    {{ action }}
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  props: {
+    to: {
+      type: String,
+      default: ''
+    },
+    action: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    onClick ($event) {
+      this.$emit("click", $event);
+    }
+  }
+}
+</script>
+

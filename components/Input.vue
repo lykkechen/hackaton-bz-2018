d5c878c3d93f9ec5bed9fd090c37c36944052b2e
Samantha Baita <baita.samantha@gmail.com>
2018-11-16 22:26:24 +0100

    :sparkles: Add input component

---
+++ components/Input.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <label
+      v-if="label"
+      class="label">
+      {{ label }}
+    </label>
+    <input
+      class="input"
+      type="text"
+      :placeholder="placeholder"
+      @input="onInput">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    label: {
+      type: String,
+      dafault: null
+    }
+  },
+  methods: {
+    onInput() {
+      $emit('input', $event.target.value)
+    }
+  }
+}
+</script>
+

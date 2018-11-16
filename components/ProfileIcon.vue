bcfd6a4399b3e51ee8ef4db3ae450fe8577333b3
Samantha Baita <baita.samantha@gmail.com>
2018-11-16 23:54:55 +0100

    :sparkles: Add profile icon component

---
+++ components/ProfileIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <figure
+      class="image is-64x64">
+      <img
+        :src="image"
+        class="is-rounded">
+    </figure>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    image: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+

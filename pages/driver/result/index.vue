9bfdbf176f7a29c061fc731b5c3c704bf0dbdafa
lykkechen <luisachen95@gmail.com>
2018-11-16 23:40:37 +0100

    Add passnger notification in driver result page

--- pages/driver/result/index.vue
+++ pages/driver/result/index.vue
@@ -1,21 +1,51 @@
 <template>
-  <div>
-    <div class="content">
-      <h1>Riepilogo</h1>
-      <p>Partenza {{ driver.from }}</p>
-      <p>Arrivo {{ driver.to }}</p>
-      <p>Ore {{ driver.time }}</p>
-      <p>Posti {{ driver.places }}</p>
+  <section class="section">
+    <div class="container">
+      <div>
+        <div>
+          <div class="content">
+            <h1>Riepilogo</h1>
+            <p>Partenza {{ driver.from }}</p>
+            <p>Arrivo {{ driver.to }}</p>
+            <p>Ore {{ driver.time }}</p>
+            <p>Posti {{ driver.places }}</p>
+          </div>
+          <div
+            v-if="isPassangerNotificationVisible"
+            class="notification">
+            <button class="delete"/>
+            <div class="content">
+              <h2>Nuova richiesta</h2>
+              <p>Nome: {{ passenger.name }}</p>
+              <p>Cognome: {{ passenger.surname }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      isPassangerNotificationVisible: false
+    }
+  },
   computed: {
     driver () {
       return this.$store.getters['driver']
+    },
+    passenger () {
+      const pass = this.$store.getters['passengers']
+      return pass[0]
     }
+  },
+  created () {
+    setTimeout(() => {
+      this.isPassangerNotificationVisible = true
+    }, 2000)
   }
 }
 </script>
--- store/index.js
+++ store/index.js
@@ -4,11 +4,20 @@ import Vuex from 'vuex'
 const createStore = () => {
   return new Vuex.Store({
     state: {
-      driver: {}
+      driver: {},
+      passengers: [
+        {
+          name: 'Mario',
+          surname: 'Rossi'
+        }
+      ]
     },
     mutations: {
       CREATE_DRIVER_ITINERARY (state, driver) {
         state.driver = driver
+      },
+      CREATE_PASSENGER_REQUEST (state, passenger) {
+        state.passengers.push(passenger)
       }
     },
     actions: {
@@ -16,6 +25,9 @@ const createStore = () => {
     getters: {
       driver (state) {
         return state.driver
+      },
+      passengers (state) {
+        return state.passengers
       }
     }
   })

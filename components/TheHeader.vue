d7b210e3798a6f804215a9d340a3adc1166b83a7
Samantha Baita <baita.samantha@gmail.com>
2018-11-16 23:55:12 +0100

    :sparkles: Add header component

---
+++ components/TheHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="navbar is-transparent">
+    <div class="navbar-brand">
+      <nuxt-link
+        to="/">
+        <profile-icon
+          image="/images/random-guy.jpeg"/>
+      </nuxt-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+import ProfileIcon from '@/components/ProfileIcon'
+export default {
+  components: {
+    ProfileIcon
+  }
+}
+</script>
+

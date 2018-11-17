c8b2a0760e11ec8ec7b8c7f19abb7fe42d225f2f
davidegiovanni <davidegiovanni96@gmail.com>
2018-11-17 10:17:55 +0100

    :sparkles: Styles update

--- pages/impact.vue
+++ pages/impact.vue
@@ -1,22 +1,37 @@
 <template>
   <section class="section">
     <div class="container">
-      <div class="columns">
+      <div class="columns has-text-centered">
         <div class="column">
           <base-card>
-            <div class="content">
-              <h1>This is your travel code: </h1>
-              <h4>1110202</h4>
-              <h3>People met</h3>
-              <p>20</p>
-              <br>
-              <h3>Traffic reduced</h3>
-              <p>10%</p>
-              <br>
-              <h3>CO2 reduced</h3>
-              <p>10%</p>
+            <div class="content has-text-centered">
+              <h3>This is the code of your carpool travel </h3>
+              <p>This code represent just this unique travel and its data saved in the OpenData Hub Sudtirol</p>
+              <h1 style="color: #20af48; margin-top: 0;">1110202</h1>
+              <hr>
+              <div
+                class="card is-shadowless"
+                style="background-color: #f5f5f5;">
+                <div class="card-content">
+                  <div class="columns">
+                    <div class="column">
+                      <p>People met</p>
+                      <h2>20</h2>
+                    </div>
+                    <div class="column">
+                      <p>Traffic reduced</p>
+                      <h1>10%</h1>
+                    </div>
+                    <div class="column">
+                      <p>CO2 reduced</p>
+                      <h1>10%</h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </base-card>
+          <br>
           <nuxt-link to="/">
             <base-button
               action="Go to home"/>
@@ -37,3 +52,13 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+ hr {
+    width: 100px;
+    border: 2px solid black;
+    margin-left: auto;
+    margin-right: auto;
+  }
+</style>
+

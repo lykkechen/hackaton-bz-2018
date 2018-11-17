8b8739bc94c59ffd37a3e7bdfb24c3ef5056465b
Samantha Baita <baita.samantha@gmail.com>
2018-11-17 02:08:46 +0100

    :sparkles: Add impact page content

--- pages/driver/impact/index.vue
+++ pages/driver/impact/index.vue
@@ -3,6 +3,16 @@
     <div class="container">
       <div class="content">
         <h1>Impact</h1>
+        <br>
+        <h4>People met</h4>
+        <p>20</p>
+        <br>
+        <h4>Traffic reduced</h4>
+        <p>30%</p>
+        <br>
+        <h4>CO2 reduced</h4>
+        <p>212121</p>
+        <br>
       </div>
     </div>
   </section>

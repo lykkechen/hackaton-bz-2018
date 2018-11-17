ed9ab60eda2a1b40e119912cb919df69c2767932
Samantha Baita <baita.samantha@gmail.com>
2018-11-17 07:48:50 +0100

    :sparkles: Update code cards

--- pages/coupons.vue
+++ pages/coupons.vue
@@ -1,24 +1,21 @@
 <template>
   <section class="section">
     <div class="container">
+      <div class="content">
+        <h1>Your codes</h1>
+      </div>
       <div class="columns is-multiline">
-        <div class="column is-full">
-          <div class="box">
+        <div
+          v-for="code in codes"
+          :key="code"
+          class="column is-half">
+          <base-card>
             <div class="content">
-              <h1>
-                Coupon #1
-              </h1>
+              <h1>Code: #{{ code }}</h1>
+              <base-button
+                action="Use it"/>
             </div>
-          </div>
-        </div>
-        <div class="column is-full">
-          <div class="box">
-            <div class="content">
-              <h1>
-                Coupon #2
-              </h1>
-            </div>
-          </div>
+          </base-card>
         </div>
       </div>
     </div>
@@ -27,9 +24,20 @@
 
 <script>
 import BaseCard from '@/components/BaseCard'
+import BaseButton from '@/components/BaseButton'
 export default {
   components: {
-    BaseCard
+    BaseCard,
+    BaseButton
+  },
+  data() {
+    return {
+      codes: [
+        '124545454',
+        '432432432',
+        "143243243"
+      ]
+    }
   }
 }
 </script>

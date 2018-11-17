62281b507552b2133160ab3df1d1506aa31ee058
Samantha Baita <baita.samantha@gmail.com>
2018-11-17 06:54:09 +0100

    :sparkles: Add impact page

---
+++ pages/driver/impact/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column">
+          <base-card>
+            <div class="content">
+              <h3>People met</h3>
+              <p>20</p>
+              <br>
+              <h3>Traffic reduced</h3>
+              <p>10%</p>
+              <br>
+              <h3>CO2 reduced</h3>
+              <p>10%</p>
+            </div>
+          </base-card>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import BaseCard from '@/components/BaseCard'
+export default {
+  components: {
+    BaseCard
+  }
+}
+</script>

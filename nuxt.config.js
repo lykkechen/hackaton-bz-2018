7329b566326fc83ef0d79a7c6364af6db2da91ba
davidegiovanni <davidegiovanni96@gmail.com>
2018-11-17 12:00:08 +0100

    :sparkles: Fix head title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,11 +7,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'PollMe',
+    title: 'PoolMe',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'PollMe' }
+      { hid: 'description', name: 'description', content: 'PoolMe' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
